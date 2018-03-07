bb2b4e18953548a6aa581200866abd3bd74ffbc2
ougato <ougato@gmail.com>
2018-03-07 09:27:38 +0800

    change list class

--- assets/resources/Script/Core/Struct/List.js
+++ assets/resources/Script/Core/Struct/List.js
@@ -1,41 +1,38 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
-
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+/**
+ * 链表节点
+ */
+let ListNode = cc.Class({
+    /**
+     * 构造
+     */
+    ctor() {
+        // 数据
+        this.m_objData = null;
+        // 上节点
+        this.m_objPrev = null;
+        // 下节点
+        this.m_objNext = null;
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
-
-    },
-
-    // update (dt) {},
 });
+
+/**
+ * 链表类
+ */
+
+let List = cc.Class({
+
+    /**
+     * 构造
+     */
+    ctor() {
+        // 链表 列表
+        this.m_node = null;
+    },
+
+
+
+
+});
+
+module.exports = List;
